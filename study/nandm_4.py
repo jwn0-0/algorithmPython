73c9f5233a63aed295718da756057e8efea8d771
서장원 <jwseo98@naver.com>
2023-05-05 01:11:21 +0900

    baekjoon #15652 commit

---
+++ study/nandm_4.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int, input().split())
+answer = []
+yeah = "yeah"
+
+def dfs(start):
+    if len(answer) == M: #M길이랑 같으면 답출력
+        print(*answer) #언패킹
+        return
+
+    for i in range(start, N+1): #1,2
+        answer.append(i) #수더해줌
+        dfs(i) #옆에 숫자 계산용
+
+        answer.pop() #answer 초기화
+
+dfs(1)
+
+
